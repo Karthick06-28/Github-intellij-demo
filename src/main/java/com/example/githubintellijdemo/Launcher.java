b5d33c279cee2f44b163b7a9b935010efd63d329
Karthick06-28 <karthickyas0619@gmail.com>
2025-09-30 13:41:24 +0530

    init commit

---
+++ src/main/java/com/example/githubintellijdemo/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.githubintellijdemo;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
